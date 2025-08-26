882d5822de67459550a909cccd9dfad8d0aba0b4
Rohan12rt <rohanangarchoudhary@gmail.com>
2025-08-26 18:03:52 +0530

    signup api

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,46 +1,47 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from '@/models/userModel';
-import { NextResponse , NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import bcryptjs from "bcryptjs";
 import { sendEmail } from "@/helpers/mailer";
 
 connect()
 
-export async function POST(request:NextRequest){
-     try {
-         const reqBody = request.json();
-         const {username , email , password }    = reqBody ;
-          // validation
-          console.log(reqBody);
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    // validation
+    console.log(reqBody);
 
-          const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-          if(user){
-            return NextResponse.json({error:'user already exist'} , {status:400})
-          }
-            
-          const salt = await bcryptjs.genSalt(10);
-          const hashPassword = await bcryptjs.hash(password,salt);
-          const newUser = new User({
-            username,
-            email,
-            password:hashPassword
-          })
+    if (user) {
+      console.log('Error:--  ' + 'user already exist');
+      return NextResponse.json({ error: 'user already exist' }, { status: 400 })
+    }
 
-          const savedUser = await newUser.save();
-          console.log(savedUser);
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword
+    })
 
-          //   send verification email
-          await sendEmail({email , emailType:"VERIFY" , userId:savedUser._id});
+    const savedUser = await newUser.save();
+    console.log(savedUser);
 
-          return NextResponse.json({
-            message:'user register successfully',
-            success:true,
-            savedUser
-          })
+    //   send verification email
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
+
+    return NextResponse.json({
+      message: 'user register successfully',
+      success: true,
+      savedUser
+    })
 
 
-     } catch (error:any) {
-        return NextResponse.json({error:error.message}, {status:500})
-     }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
 }
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -30,24 +30,30 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
     //   },
     // });
 
-          var transport = nodemailer.createTransport({
-            host: "live.smtp.mailtrap.io",
-            port: 587,
-            auth: {
-              user: "api",
-              pass: "<YOUR_API_TOKEN>"
-            }
-          });
+
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "5075fc90413a90", //❌
+        pass: "143428568854ce"
+      }
+    });
 
     const mailOptions = {
       from: "rohan@gmail.com",
       to: email,
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-      html: "<b>Hello world?</b>",
+      html: `<p> Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}"> here </a> to
+                 ${emailType === "VERIFY" ? "Verify your email" : "Reset your password"}
+                 or copy and paste the link below in your browser.
+                 <br>
+                 ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+             </p>`,
     };
 
-    const mailResponse = await transporter.sendMail(mailOptions);
+    const mailResponse = await transport.sendMail(mailOptions);
     return mailResponse;
   } catch (error: any) {
     throw new Error(error.message);
